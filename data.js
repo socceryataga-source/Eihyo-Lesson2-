8b617f97fb10758f473a3f61084c68aad6c4fd35
socceryataga-source <socceryataga@gmail.com>
2026-04-20 13:52:44 +0900

    Add files via upload

--- data.js
+++ data.js
@@ -1,79 +1,57 @@
-window.PICTO_DATA = [
+const data = [
   {
-    "id": 1,
-    "image": "images/img001.png",
-    "japanese": "ドアが閉まるのが聞こえました。",
-    "english": "I heard the door close.",
-    "hint": "I heard the door (        )."
+    "img": "img001.png",
+    "jp": "ドアが閉まるのが聞こえました。",
+    "en": "I heard the door close."
   },
   {
-    "id": 2,
-    "image": "images/img002.png",
-    "japanese": "私は図書館から何冊か本を借りました。",
-    "english": "I borrowed some books from the library.",
-    "hint": "I (        ) some books from the library."
+    "img": "img002.png",
+    "jp": "私は図書館から何冊か本を借りました。",
+    "en": "I borrowed some books from the library."
   },
   {
-    "id": 3,
-    "image": "images/img003.png",
-    "japanese": "彼は昨夜スマートフォンをなくしました。",
-    "english": "He lost his smartphone last night.",
-    "hint": "He (        ) his smartphone last night."
+    "img": "img003.png",
+    "jp": "彼は昨夜スマートフォンをなくしました。",
+    "en": "He lost his smartphone last night."
   },
   {
-    "id": 4,
-    "image": "images/img004.png",
-    "japanese": "私はその時間に友人と昼食を食べていました。",
-    "english": "I was having lunch with my friend at that time.",
-    "hint": "I was (        ) lunch with my friend at that time."
+    "img": "img004.png",
+    "jp": "私はその時間に友人と昼食を食べていました。",
+    "en": "I was having lunch with my friend at that time."
   },
   {
-    "id": 5,
-    "image": "images/img005.png",
-    "japanese": "私の姉は夕飯を食べずに電話で話していました。",
-    "english": "My sister was talking on the phone without having dinner.",
-    "hint": "My sister was (        ) on the phone without having dinner."
+    "img": "img005.png",
+    "jp": "私の姉は夕飯を食べずに電話で話していました。",
+    "en": "My sister was talking on the phone without having dinner."
   },
   {
-    "id": 6,
-    "image": "images/img006.png",
-    "japanese": "私の犬はソファで寝ていました。",
-    "english": "My dog was sleeping on the sofa.",
-    "hint": "My dog was (        ) on the sofa."
+    "img": "img006.png",
+    "jp": "私の犬はソファで寝ていました。",
+    "en": "My dog was sleeping on the sofa."
   },
   {
-    "id": 7,
-    "image": "images/img007.png",
-    "japanese": "私が帰宅したとき，彼はテレビを観ていました。",
-    "english": "He was watching TV when I came home.",
-    "hint": "He was (        ) TV when I came home."
+    "img": "img007.png",
+    "jp": "私が帰宅したとき，彼はテレビを観ていました。",
+    "en": "He was watching TV when I came home."
   },
   {
-    "id": 8,
-    "image": "images/img008.png",
-    "japanese": "私が彼女を見かけたとき，彼女は泣いていました。",
-    "english": "She was crying when I saw her.",
-    "hint": "She was (        ) when I saw her."
+    "img": "img008.png",
+    "jp": "私が彼女を見かけたとき，彼女は泣いていました。",
+    "en": "She was crying when I saw her."
   },
   {
-    "id": 9,
-    "image": "images/img009.png",
-    "japanese": "私が昼寝をしている間に，私の母が電話を私にかけました。",
-    "english": "My mother called me while I was taking a nap.",
-    "hint": "My mother (        ) me while I was taking a nap."
+    "img": "img009.png",
+    "jp": "私が昼寝をしている間に，私の母が電話を私にかけました。",
+    "en": "My mother called me while I was taking a nap."
   },
   {
-    "id": 10,
-    "image": "images/img010.png",
-    "japanese": "彼女の息子はその公園でよく野球をしました。",
-    "english": "Her son used to play baseball in that park.",
-    "hint": "Her son used to (        ) baseball in that park."
+    "img": "img010.png",
+    "jp": "彼女の息子はその公園でよく野球をしました。",
+    "en": "Her son used to play baseball in that park."
   },
   {
-    "id": 11,
-    "image": "images/img011.png",
-    "japanese": "バス停の前に大きな木があった。",
-    "english": "There used to be a big tree in front of the bus stop.",
-    "hint": "There used to (        ) a big tree in front of the bus stop."
+    "img": "img011.png",
+    "jp": "バス停の前に大きな木があった。",
+    "en": "There used to be a big tree in front of the bus stop."
   }
 ];
